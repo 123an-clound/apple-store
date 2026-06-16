1939661729a2215fc1b03440f2cdaaca4f42b663
123an-clound <phamtuanan3939@gmail.com>
2026-06-16 18:32:31 +0700

    update

--- store/lib/constants.js
+++ store/lib/constants.js
@@ -1,7 +1,7 @@
-export const HOTLINE = '0792345679';
-export const HOTLINE_DISPLAY = '0792 345 679';
+export const HOTLINE = '0123456789';
+export const HOTLINE_DISPLAY = '0123 456 789';
 export const TEL_URL = `tel:${HOTLINE}`;
 export const ZALO_URL = `https://zalo.me/${HOTLINE}`;
 
-export const ZALO_TRAGOP = '84792345679';
+export const ZALO_TRAGOP = '0123456789';
 export const ZALO_TRAGOP_URL = `https://zalo.me/${ZALO_TRAGOP}`;
